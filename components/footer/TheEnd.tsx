be653b1a9f65d667a428004021deb41217edcd9c
SzilardZ <szilard.zr@gmail.com>
2023-02-03 15:42:17 +0100

    create the-end component

---
+++ components/footer/TheEnd.tsx
@@ -0,0 +1,11 @@
+import styles from './TheEnd.module.css';
+
+const TheEnd = () => {
+  return (
+    <div className={styles.text}>
+      <p>the end</p>
+    </div>
+  );
+};
+
+export default TheEnd;
--- components/movies/MovieDetails.tsx
+++ components/movies/MovieDetails.tsx
@@ -5,6 +5,7 @@ import { MovieModel, SimpleMovieModel } from '../../types/movieTypes';
 import { useDispatch, useSelector } from 'react-redux';
 import { addMovie, removeMovie } from '../../store/favMovies/favMoviesSlice';
 import RoleActor from './RoleActor';
+import TheEnd from '../footer/TheEnd';
 
 interface MovieDetailProps {
   movie: MovieModel;
@@ -92,6 +93,7 @@ const MovieDetails = (props: MovieDetailProps) => {
           </section>
         </div>
       </main>
+      <TheEnd />
     </Fragment>
   );
 };
