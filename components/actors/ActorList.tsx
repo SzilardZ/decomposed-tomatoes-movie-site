ba3d85ddf11124b2a46968567a728af2f5b676bc
SzilardZ <szilard.zr@gmail.com>
2023-02-08 14:21:34 +0100

    handle if there is no results for actors

--- components/actors/ActorList.tsx
+++ components/actors/ActorList.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import { ActorType } from '../../types/actorTypes';
 import Navbar from '../navbar/Navbar';
+import NoResults from '../no-results/NoResults';
 import ActorItem from './ActorItem';
 
 import styles from './ActorList.module.css';
@@ -9,17 +10,25 @@ interface ActorListProps {
   actors: ActorType[];
 }
 
-const ActorList = (props: ActorListProps) => {
+const ActorList = ({ actors }: ActorListProps) => {
+  let content;
+  if (actors.length === 0) {
+    content = <NoResults type='actor' />;
+  }
+  if (actors.length > 0) {
+    content = (
+      <ul className={styles.actors}>
+        {actors.map(actor => (
+          <ActorItem key={actor.id} id={actor.id} name={actor.name} />
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <Fragment>
       <Navbar />
-      <div className={styles.container}>
-        <ul className={styles.actors}>
-          {props.actors.map(actor => (
-            <ActorItem key={actor.id} id={actor.id} name={actor.name} />
-          ))}
-        </ul>
-      </div>
+      <div className={styles.container}>{content}</div>
     </Fragment>
   );
 };
