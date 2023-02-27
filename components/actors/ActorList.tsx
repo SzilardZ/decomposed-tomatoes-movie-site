424caa0d08e9f216f7e20bdd65088deef1f963bf
SzilardZ <szilard.zr@gmail.com>
2023-02-27 18:53:17 +0100

    add searchfield, functionality and refine design for searched actor list

--- components/actors/ActorList.tsx
+++ components/actors/ActorList.tsx
@@ -5,12 +5,15 @@ import styles from './ActorList.module.css';
 import Navbar from '../navbar/Navbar';
 import NoResults from '../no-results/NoResults';
 import ActorItem from './ActorItem';
+import SearchField from '../search-field/SearchField';
+import { capitalizeFirstLetter } from '../../util/helpers';
 
 interface ActorListProps {
   actors: ActorType[];
+  searchedActor: string;
 }
 
-const ActorList = ({ actors }: ActorListProps) => {
+const ActorList = ({ actors, searchedActor }: ActorListProps) => {
   let content;
   if (actors.length === 0) {
     content = <NoResults type='actor' />;
@@ -29,7 +32,15 @@ const ActorList = ({ actors }: ActorListProps) => {
     <Fragment>
       <Navbar />
       <div className={styles['outer-container']}>
-        <div className={styles['inner-container']}>{content}</div>
+        <div className={styles['inner-container']}>
+          <div className={styles['title-search-container']}>
+            <h3 className={styles['sub-title']}>
+              Results for: <span>{capitalizeFirstLetter(searchedActor)}</span>
+            </h3>
+            <SearchField type='actor' />
+          </div>
+          <div>{content}</div>
+        </div>
       </div>
     </Fragment>
   );
