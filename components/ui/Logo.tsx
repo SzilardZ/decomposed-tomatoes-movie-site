df56a5f5fec6c54c698cf8956d6a5bca1aca5464
SzilardZ <szilard.zr@gmail.com>
2024-01-10 02:55:05 +0100

    refine logo 2

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,20 +1,12 @@
 import Link from 'next/link';
-import tomato from '../../public/tomato.png';
 
 import styles from './Logo.module.css';
-import Image from 'next/image';
 
 const Logo = () => {
   return (
     <Link href='/'>
       <div className={styles.logo}>
-        <div>
-          decomposed t
-          <span className=''>
-            <Image src={tomato} width={32} height={32} alt='tomato' />
-          </span>
-          matoes.
-        </div>
+        <div>decomposed tomatoes.</div>
       </div>
     </Link>
   );
